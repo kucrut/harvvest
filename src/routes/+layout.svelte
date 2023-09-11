c13e413ea7c8f9c290db806123e6aac651465eda
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-12 00:30:44 +0700

    Add root layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<nav>
+	<!-- TODO: Don't show on login page. -->
+	{#if ! $page.data.user}
+		<a href="/login">Log In</a>
+	{/if}
+
+	{#if $page.data.user}
+		<form action="/logout" method="POST">
+			<button type="submit">Log Out</button>
+		</form>
+	{/if}
+</nav>
+
+<main>
+  <slot />
+</main>
