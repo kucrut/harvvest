cf0fc852e9143f3467b111655349ebd64d338467
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-12 22:47:50 +0700

    Move wp_login() to utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -15,6 +15,62 @@ export function logout( cookies ) {
 }
 
 /**
+ * Log in to WordPress via REST API
+ *
+ * @param {string} url      WordPress URL.
+ * @param {string} username Username or email.
+ * @param {string} password Password.
+ *
+ * @return {Promise<import('$types').Session|Error>} User or error object.
+ */
+export async function wp_login( url, username, password ) {
+	try {
+		const response = await fetch( `${ url }/wp-json/jwt-auth/v1/token`, {
+			method: 'POST',
+			body: JSON.stringify( {
+				username,
+				password,
+			} ),
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+			},
+		} );
+
+		if ( ! response.ok ) {
+			/** @type {string} */
+			let message;
+
+			if ( response.status === 403 ) {
+				const result = await response.json();
+				message = result.message;
+			} else {
+				message = response.statusText;
+			}
+
+			throw new Error( message );
+		}
+
+		// TODO: Validate.
+		const data = await response.json();
+
+		return {
+			email: data.user_email,
+			name: data.user_display_name || data.user_nicename,
+			token: data.token,
+			url: `${ url }/wp-json`,
+		};
+	} catch ( err ) {
+		if ( err instanceof Error ) {
+			return err;
+		}
+
+		return new Error( 'Unknown error occured.' );
+	}
+}
+
+
+/**
  * Upload image to WordPress
  *
  * @param {string}   url   WordPress URL.
