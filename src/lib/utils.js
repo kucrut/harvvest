913a4b4b3a107260420f073db848042bef2e63f6
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-12 22:44:44 +0700

    Add helper: wp_upload()

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -13,3 +13,39 @@ export function logout( cookies ) {
 
 	throw redirect( 302, '/login' );
 }
+
+/**
+ * Upload image to WordPress
+ *
+ * @param {string}   url   WordPress URL.
+ * @param {string}   token Auth token.
+ * @param {FormData} data  Form data.
+ *
+ * @return {Promise<string|Error>} Uploaded image link or error object.
+ */
+export async function wp_upload( url, token, data ) {
+	try {
+		const response = await fetch( `${ url }/wp/v2/media`, {
+			body: data,
+			method: 'POST',
+			headers: {
+				'Authorization': `Bearer ${ token }`,
+			},
+		} );
+
+		const result = await response.json();
+
+		if ( ! response.ok ) {
+			throw new Error( result?.message || response.statusText );
+		}
+
+		return result.guid.rendered;
+	} catch ( err ) {
+		if ( err instanceof Error ) {
+			return err;
+		}
+
+		return new Error( 'Unknown error occured.' );
+	}
+}
+
