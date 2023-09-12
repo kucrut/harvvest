5c3135e24b0783591db0e88176f794aeba3a99e9
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-12 21:18:07 +0700

    /logout: Use logout()

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -1,16 +1,13 @@
 import { redirect } from '@sveltejs/kit';
+import { logout } from '$lib/utils';
 
 export const load = async () => {
 	throw redirect( 302, '/' );
 };
 
+/** @type {import('./$types').Actions} */
 export const actions = {
 	default( { cookies } ) {
-		cookies.set( 'session', '', {
-			path: '/',
-			expires: new Date( 0 ),
-		} );
-
-		throw redirect( 302, '/login' );
+		logout( cookies );
 	},
 };
