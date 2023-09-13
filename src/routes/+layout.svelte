ef78b81699295b161d4d2c885afe0d7b9ec1198d
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-14 00:14:46 +0700

    Add TODO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,8 @@
 
 <AppShell>
 	<AppBar slot="header" slotDefault="place-self-center" slotTrail="place-content-end">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<!-- TODO -->
+		<svelte:fragment slot="lead"><span /></svelte:fragment>
 		<h1 class="h2">Photo Harvest</h1>
 		<svelte:fragment slot="trail">
 			{#if $page.data.user}
