6a900b720b00eb0fe1264f02336043e90c7a772b
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-14 00:10:22 +0700

    FormWrap: Switch to grid

--- src/lib/components/form-wrap.svelte
+++ src/lib/components/form-wrap.svelte
@@ -1,5 +1,7 @@
-<div class="flex justify-center items-center mx-auto transition-[width] duration-200 w-full">
+<div class="gap-y-4 grid place-items-center mx-auto transition-[width] duration-200 w-full">
+	<slot name="before" />
 	<div class="card p-4 w-full text-token space-y-4">
 		<slot/>
 	</div>
+	<slot name="after" />
 </div>
