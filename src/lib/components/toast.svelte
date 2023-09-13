df61260beba187608c8e23b517f52608ce942455
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-13 20:50:54 +0700

    Add component: Toast

---
+++ src/lib/components/toast.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	/** @type {'error' | 'success'}*/
+	export let type = 'success';
+</script>
+
+<aside class="alert variant-soft-{type}" transition:fade|local={{ duration: 200 }}>
+	<div class="alert-message">
+		<slot name="title" />
+		<slot name="message" />
+	</div>
+	<div class="alert-actions">
+		<slot name="actions">
+			<button class="btn-icon variant-filled" on:click><i class="fa-solid fa-xmark"></i></button>
+		</slot>
+	</div>
+</aside>
