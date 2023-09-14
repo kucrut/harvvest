e891608d807d6ad604064f38ed5e82cb7ffde192
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-14 14:07:29 +0700

    Main page: Display site info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,6 +57,7 @@
 
 <div class="p-4 md:p-10 space-y-4">
 	<h1 class="h3 italic text-center">Hello, <strong>{$page.data.user.name}!</strong></h1>
+	<p class="text-center">You are logged in to <span class="font-mono">{$page.data.user.url}</span>.</p>
 
 	{#if form?.error}
 		<!-- TODO: Toast -->
