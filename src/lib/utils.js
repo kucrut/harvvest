5bfcdc4794e83848a3d2e7cb5319d3c549b246ee
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-05 12:26:47 +0700

    wp_upload: Throw errors

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -92,37 +92,29 @@ export async function wp_login( url, username, password ) {
  * @param {string}   token   Auth token.
  * @param {FormData} data    Form data.
  *
- * @return {Promise<string|Error>} Uploaded image link or error object.
+ * @return {Promise<string>} Uploaded image link.
  */
 export async function wp_upload( api_url, token, data ) {
-	try {
-		const response = await fetch( `${ api_url }/wp/v2/media`, {
-			body: data,
-			method: 'POST',
-			headers: {
-				Authorization: `Bearer ${ token }`,
-			},
-		} );
+	const response = await fetch( `${ api_url }/wp/v2/media`, {
+		body: data,
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${ token }`,
+		},
+	} );
 
-		if ( response.ok ) {
-			const result = await response.json();
-			return result.guid.rendered;
-		}
-
-		const content_type = response.headers.get( 'Content-Type' );
-
-		if ( content_type?.startsWith( 'text/' ) ) {
-			throw new Error( await response.text() );
-		}
-
-		// TODO: Validate.
-		const json = await response.json();
-		throw new Error( json.message );
-	} catch ( err ) {
-		if ( err instanceof Error ) {
-			return err;
-		}
-
-		return new Error( 'Unknown error occured.' );
+	if ( response.ok ) {
+		const result = await response.json();
+		return result.guid.rendered;
 	}
+
+	const content_type = response.headers.get( 'Content-Type' );
+
+	if ( content_type?.startsWith( 'text/' ) ) {
+		throw new Error( await response.text() );
+	}
+
+	// TODO: Validate.
+	const json = await response.json();
+	throw new Error( json.message );
 }
