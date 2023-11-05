14c34eee3ad98fe0348ea3b8c0a1f3dbdba7ac60
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-06 06:18:43 +0700

    Server hooks: Validate token

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,4 @@
-import { delete_session_cookies, validate_session } from '$lib/utils.server.js';
+import { delete_session_cookies, validate_session, validate_token } from '$lib/utils.server.js';
 
 export const handle = async ( { event, resolve } ) => {
 	const session_cookie = event.cookies.get( 'session' );
@@ -8,7 +8,9 @@ export const handle = async ( { event, resolve } ) => {
 	}
 
 	try {
-		const { email, name, url } = validate_session( session_cookie );
+		const session = validate_session( session_cookie );
+		await validate_token( session );
+		const { email, name, url } = session;
 		event.locals.user = { email, name, url };
 	} catch {
 		delete_session_cookies( event.cookies );
