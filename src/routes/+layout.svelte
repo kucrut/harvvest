11582ce64fca0a3f08521cfdbe70381234236b79
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-05 20:29:15 +0700

    Update todo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,14 @@
 	import '../app.postcss';
 </script>
 
-
 <AppShell>
-	<AppBar gridColumns="grid-cols-[1fr_auto_1fr]" slot="header" slotDefault="place-self-center" slotTrail="place-content-end">
-		<!-- TODO -->
+	<AppBar
+		gridColumns="grid-cols-[1fr_auto_1fr]"
+		slot="header"
+		slotDefault="place-self-center"
+		slotTrail="place-content-end"
+	>
+		<!-- TODO: App icon -->
 		<svelte:fragment slot="lead"><span /></svelte:fragment>
 		<h1 class="h2">Photo Harvest</h1>
 		<svelte:fragment slot="trail">
@@ -21,4 +25,3 @@
 
 	<slot />
 </AppShell>
-
