fd29f839222f2640db44311c8b3e9b87641f772f
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-05 15:02:45 +0700

    Add helper: get_error_message()

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,6 +3,33 @@ import { session_schema, wp_media_item_schema, wp_user_schema } from './schema';
 import { ZodError } from 'zod';
 
 /**
+ * Get error message
+ *
+ * @param {unknown}  error    Error object, whatever.
+ * @param {string}   fallback Fallback message if the error is unrecognized.
+ * @param {boolean=} dump     Wheter to dump error if the error is unrecognized. (Defaults to false).
+ *
+ * @return {string} Error message.
+ */
+export function get_error_message( error, fallback, dump = false ) {
+	/** @type {string} */
+	let message;
+
+	if ( error instanceof Error || error instanceof ZodError ) {
+		message = error.message;
+	} else {
+		message = fallback;
+
+		if ( dump ) {
+			// eslint-disable-next-line no-console
+			console.error( error );
+		}
+	}
+
+	return message;
+}
+
+/**
  * Log out
  *
  * @param {import('@sveltejs/kit').Cookies} cookies Coooooookiiiiieeees.
