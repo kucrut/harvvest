b00b859c0b5c63b5ff69c7d49fe722a827c81f5b
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-05 07:53:12 +0700

    Add schema: session

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -7,3 +7,10 @@ export const user_schema = z.object( {
 } );
 
 /** @typedef {z.infer<user_schema>} User */
+
+export const session_schema = user_schema.extend( {
+	api_url: z.string().url(),
+	token: z.string(),
+} );
+
+/** @typedef {z.infer<session_schema>} Session */
