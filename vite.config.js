042c54a0e9bfdab93bd75c8424c6d9cfda11b929
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-05 21:59:29 +0700

    Disable pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+// import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig( {
-	plugins: [ sveltekit(), SvelteKitPWA() ],
+	plugins: [ sveltekit() ],
 } );
