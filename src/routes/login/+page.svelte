6d447e82a5d06a9bab11add8547674a0a446637e
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-06 08:37:02 +0700

    Login page: Handle submission on client

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,26 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
 	import FormWrap from '$lib/components/form-wrap.svelte';
 	import Toast from '$lib/components/toast.svelte';
 
 	/** @type {import('./$types').ActionData}*/
 	export let form;
+	let error_message = form?.error || '';
 
-	let should_show_error_message = true;
+	/** @type {import('@sveltejs/kit').SubmitFunction}*/
+	const handle_submit = () => {
+		return ( { result } ) => {
+			if ( result.type === 'redirect' ) {
+				goto( result.location, { invalidateAll: true } );
+				return;
+			}
+
+			if ( result.type === 'failure' ) {
+				error_message = result.data?.message;
+			}
+		};
+	};
 </script>
 
 <svelte:head>
@@ -16,15 +30,15 @@
 <div class="p-4 md:p-10 space-y-4">
 	<h1 class="h3 text-center">Log In</h1>
 
-	{#if form?.error && should_show_error_message}
-		<Toast type="error" on:click={() => ( should_show_error_message = false )}>
+	{#if error_message}
+		<Toast type="error" on:click={() => ( error_message = '' )}>
 			<h2 class="h3" slot="title">Error</h2>
 			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-			<p slot="message">{@html form.message}</p>
+			<p slot="message">{@html error_message}</p>
 		</Toast>
 	{/if}
 
-	<form method="POST" use:enhance>
+	<form method="POST" use:enhance={handle_submit}>
 		<FormWrap>
 			<label class="label">
 				<span>WordPress URL</span>
