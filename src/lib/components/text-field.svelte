b9f673558e1afad3a5b04fc8fc77ea0f49dbfc86
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-07 21:10:17 +0700

    Add component: TextField

---
+++ src/lib/components/text-field.svelte
@@ -0,0 +1,14 @@
+<script>
+	/** @type {string} */
+	export let label;
+	export let multiline = false;
+</script>
+
+<label class="label">
+	<span>{label}</span>
+	{#if multiline}
+		<textarea class="textarea" {...$$restProps} />
+	{:else}
+		<input class="input" {...$$restProps} />
+	{/if}
+</label>
