f2e7571900df5f9b3c2cd7d96d06fec28d915b66
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-07 20:55:57 +0700

    Spinner: Accept class prop

--- src/lib/components/spinner.svelte
+++ src/lib/components/spinner.svelte
@@ -1,5 +1,10 @@
+<script>
+	let cls = 'text-surface-400';
+	export { cls as class };
+</script>
+
 <svg
-	class="motion-safe:animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+	class="motion-safe:animate-spin -ml-1 mr-3 h-5 w-5 {cls}"
 	xmlns="http://www.w3.org/2000/svg"
 	fill="none"
 	viewBox="0 0 24 24"
