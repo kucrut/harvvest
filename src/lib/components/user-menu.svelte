28742e44aaae8323724eadac0a94e31b9ca720d3
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-07 17:12:43 +0700

    Add component: UserMenu

---
+++ src/lib/components/user-menu.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { Avatar, popup } from '@skeletonlabs/skeleton';
+
+	/** @type {import("$lib/schema").User}*/
+	export let user;
+
+	/** @type {import('@skeletonlabs/skeleton').PopupSettings}*/
+	const user_menu_settings = {
+		closeQuery: 'li',
+		event: 'focus-click',
+		placement: 'bottom',
+		target: 'user_menu',
+	};
+</script>
+
+<button use:popup={user_menu_settings}>
+	<Avatar src={user.avatar_url} rounded="rounded-md" width="w-8" />
+</button>
+
+<div class="card w-48 p-2 shadow-xl space-y-2 !mx-0" data-popup="user_menu">
+	<p class="px-4 py-2">Hello, <span class="font-semibold">{user.name}</span>!</p>
+	<hr />
+	<ul class="list-nav">
+		<li>
+			<form action="/logout" method="POST">
+				<button class="h-full w-full" type="submit">Log Out</button>
+			</form>
+		</li>
+	</ul>
+	<div class="arrow bg-surface-100-800-token" />
+</div>
