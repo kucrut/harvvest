fa60bfdb8b6ef2821549534238228a60946211c2
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-07 21:38:45 +0700

    SubmitField: Accept label

--- src/lib/components/submit-field.svelte
+++ src/lib/components/submit-field.svelte
@@ -2,10 +2,11 @@
 	import Spinner from './spinner.svelte';
 
 	export let is_submitting = false;
+	export let label = 'Submit';
 </script>
 
 <p class="flex items-center gap-4">
-	<button class="btn variant-filled" disabled={is_submitting} type="submit"><slot /></button>
+	<button class="btn variant-filled" disabled={is_submitting} type="submit">{label}</button>
 	{#if is_submitting}
 		<Spinner />
 	{/if}
