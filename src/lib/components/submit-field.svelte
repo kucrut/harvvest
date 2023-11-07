133f4bdd1544432be28f0ad4fec589b372aaa52e
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-07 20:59:56 +0700

    Add component: SubmitField

---
+++ src/lib/components/submit-field.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Spinner from './spinner.svelte';
+
+	export let is_submitting = false;
+</script>
+
+<p class="flex items-center gap-4">
+	<button class="btn variant-filled" disabled={is_submitting} type="submit"><slot /></button>
+	{#if is_submitting}
+		<Spinner />
+	{/if}
+</p>
