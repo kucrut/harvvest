3ab5f45d9b5f495dace9dae4560f78b7b1669439
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-09 00:22:52 +0700

    Add component: Toast

---
+++ src/lib/components/toast.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { get_toast_store } from '$lib/stores/toast';
+	import { onMount } from 'svelte';
+	import { slide } from 'svelte/transition';
+
+	/** @type {import("$types").Toast}*/
+	export let item;
+
+	const store = get_toast_store();
+	const remove = () => store.remove( item.id );
+
+	onMount( () => {
+		if ( ! item.autohide || ( typeof item.autohide === 'number' && item.autohide < 0 ) ) {
+			return;
+		}
+
+		const timeout = typeof item.autohide === 'number' ? item.autohide : 10000;
+
+		setTimeout( remove, timeout );
+	} );
+</script>
+
+<div role="alertdialog" aria-live="polite" transition:slide|local={{ duration: 200 }}>
+	<div
+		class="toast flex justify-between items-center pointer-events-auto max-w-[640px] p-4 space-x-4 rounded-container-token shadow-lg variant-ghost-{item.type}"
+	>
+		<div class="text-base">
+			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+			{@html item.message}
+		</div>
+		<div class="toast-actions flex items-center space-x-2">
+			<button class="btn-icon btn-icon-sm variant-filled" aria-label="Dismiss" on:click={remove}>✕</button>
+		</div>
+	</div>
+</div>
