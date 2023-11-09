8778f57d655026ccb378094527746d56b661f3b9
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-09 23:01:26 +0700

    Add helper: retrieve_pwa_shared_file()

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -123,3 +123,30 @@ export async function handle_wp_rest_response( response, callback ) {
 export function remove_file_extension( filename ) {
 	return filename.split( '.' ).slice( 0, -1 ).join( '.' );
 }
+
+/**
+ * Retrieve shared file from PWA
+ *
+ * Stolen from Squoosh.
+ *
+ * @return {Promise<File>} Shared file object.
+ */
+export async function retrieve_pwa_shared_file() {
+	return new Promise( resolve => {
+		/** @param {MessageEvent} event */
+		const onmessage = event => {
+			if ( event.data.action !== 'load-image' ) {
+				return;
+			}
+
+			resolve( event.data.file );
+			navigator.serviceWorker.removeEventListener( 'message', onmessage );
+		};
+
+		navigator.serviceWorker.addEventListener( 'message', onmessage );
+
+		// This message is picked up by the service worker -
+		// it's how it knows we're ready to receive the file.
+		navigator.serviceWorker.controller?.postMessage( 'share-ready' );
+	} );
+}
