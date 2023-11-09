192308f14f3f6d1d8f73e8240bb0d3f3d35687c6
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-10 00:31:49 +0700

    Add component: Alert

---
+++ src/lib/components/alert.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	/** @type {Record<string,string>} */
+	export let meta;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<aside class="alert variant-ghost-{meta.type}">
+	<div class="alert-message">
+		<h3 class="h3">{meta.title}</h3>
+		<p>{meta.message}</p>
+	</div>
+	<div class="alert-actions">
+		<button class="btn-icon btn-icon-sm variant-filled" aria-label="Dismiss" on:click={() => dispatch( 'dismiss' )}
+			>✕</button
+		>
+	</div>
+</aside>
