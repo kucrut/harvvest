01a703cd8504913281d9248786eb9d74ad2d95bd
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-10 01:05:30 +0700

    Alert interface: Add data_to_copy

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
 export type HandleResponse< T > = ( data: unknown ) => Promise< T >;
 
+export interface AlertDataToCopy {
+	content: string;
+	label?: string;
+}
+
 export interface Alert {
+	data_to_copy?: AlertDataToCopy[];
 	message: string;
 	title: string;
 	type: 'error' | 'success';
