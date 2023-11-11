8d6b2d11d25fd6b376a95418977f8ba186949652
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-11 16:25:02 +0700

    Server hook: Inject terms to locals

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,10 @@
-import { delete_session_cookies, validate_session, validate_token } from '$lib/utils.server.js';
+import {
+	delete_session_cookies,
+	validate_session,
+	validate_token,
+	wp_get_attachment_taxonomies,
+	wp_get_taxonomy_terms,
+} from '$lib/utils.server.js';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ( { event, resolve } ) => {
@@ -8,13 +14,41 @@ export const handle = async ( { event, resolve } ) => {
 		return await resolve( event );
 	}
 
+	/** @type {import('./lib/schema').Session} */
+	let session;
+
 	try {
-		const session = validate_session( session_cookie );
+		session = validate_session( session_cookie );
 		await validate_token( session );
 		const { avatar_url, email, name, wp_url } = session;
 		event.locals.user = { avatar_url, email, name, wp_url };
 	} catch {
 		delete_session_cookies( event.cookies );
+
+		return await resolve( event );
+	}
+
+	try {
+		const taxonomies = await wp_get_attachment_taxonomies( session.api_url, session.token );
+		/** @type {import('$types').Taxonomy_Terms_Option[]} */
+		const terms = [];
+
+		for ( const tax of Object.values( taxonomies ) ) {
+			try {
+				const tax_terms = await wp_get_taxonomy_terms( tax._links[ 'wp:items' ][ 0 ].href, session.token );
+
+				terms.push( { name: tax.name, slug: tax.slug, terms: tax_terms.map( ( { id, name } ) => ( { id, name } ) ) } );
+			} catch ( err ) {
+				// eslint-disable-next-line no-console
+				console.log( err );
+				continue;
+			}
+		}
+
+		event.locals.terms = terms;
+	} catch ( error ) {
+		// eslint-disable-next-line no-console
+		console.log( error );
 	}
 
 	return await resolve( event );
