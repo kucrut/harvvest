c771ce49e4302c30c08175cc9c0ade9a4d4ef071
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-11 15:54:54 +0700

    Add wp_taxonomy_schema, wp_taxonomies_schema

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -75,6 +75,26 @@ const wp_links_schema = z.object( {
 	collection: wp_link_item_schema,
 } );
 
+export const wp_taxonomy_schema = z.object( {
+	hierarchical: z.boolean(),
+	description: z.string(),
+	name: z.string(),
+	rest_base: z.string(),
+	rest_namespace: z.string(),
+	slug: z.string(),
+	types: z.string().array(),
+	_links: z.object( {
+		'collection': wp_link_item_schema,
+		'wp:items': wp_link_item_schema,
+	} ),
+} );
+
+/** @typedef {z.infer<wp_taxonomy_schema>} WP_Taxonomy */
+
+export const wp_taxonomies_schema = z.record( wp_taxonomy_schema );
+
+/** @typedef {z.infer<wp_taxonomies_schema>} WP_Taxonomies */
+
 export const wp_user_schema = z.object( {
 	avatar_urls: z.record( z.string().url() ),
 	description: z.string(),
