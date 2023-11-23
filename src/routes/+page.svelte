685f9d9c2af0ea83122e4c392cbf99815df23719
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-11-24 03:28:56 +0700

    Upload form: Update drop zone text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -142,7 +142,7 @@
 							</div>
 						{/if}
 					</svelte:fragment>
-					<p slot="message">Click to select an image or drag and drop it here.</p>
+					<p slot="message">Click to select an image/video or drag and drop it here.</p>
 				</FileDropzone>
 				<TextField multiline required disabled={is_submitting} label="Alternative text" name="alt_text" />
 				<TextField required disabled={is_submitting} label="Caption" name="caption" />
