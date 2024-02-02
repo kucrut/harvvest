cdaeb1a4006d56130d7aedf456d94dcd66b7c888
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-02 17:11:38 +0700

    Remove obsolete code

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-// import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig( {
 	plugins: [ sveltekit() ],
