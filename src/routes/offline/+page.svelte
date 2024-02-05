358db42fcd5415f0e1fe25cac1477733cde804e0
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-05 22:52:26 +0700

    Improve offline page:
    * Redirect to homepage when online
    * Add Refresh button for manual redirect

--- src/routes/offline/+page.svelte
+++ src/routes/offline/+page.svelte
@@ -1,5 +1,14 @@
 <script>
 	import ContentWrap from '$lib/components/content-wrap.svelte';
+	import { onMount } from 'svelte';
+
+	function redirect() {
+		window.location.replace( '/' );
+	}
+
+	onMount( () => {
+		window.addEventListener( 'online', redirect );
+	} );
 </script>
 
 <svelte:head>
@@ -7,5 +16,8 @@
 </svelte:head>
 
 <ContentWrap>
-	<p class="text-center">You are offline.</p>
+	<div class="grid place-content-center gap-5">
+		<p>You are offline.</p>
+		<button class="btn variant-filled" type="button" on:click={redirect}>Refresh</button>
+	</div>
 </ContentWrap>
