b7dfa7069496e664fd22dc933cfda7450357913e
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-05 22:35:44 +0700

    Add /offline page

---
+++ src/routes/offline/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ContentWrap from '$lib/components/content-wrap.svelte';
+</script>
+
+<svelte:head>
+	<title>Photo Harvest</title>
+</svelte:head>
+
+<ContentWrap>
+	<p class="text-center">You are offline.</p>
+</ContentWrap>
