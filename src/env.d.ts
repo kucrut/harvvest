2ab7c7ae2e968e6acd8286ca03e4a35e263e5200
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-11 17:21:26 +0700

    Add env vars: APP_ID, APP_NAME

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+	readonly APP_ID: string;
+	readonly APP_NAME: string;
 	readonly ACCESS_KEYS: string;
 	readonly PUBLIC_MAX_FILE_SIZE: string;
 }
