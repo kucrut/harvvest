85f437a8f3a8d8f203560afc3cd2a190e4d27b25
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-12 11:17:13 +0700

    Remove APP_ID env var

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly APP_ID: string;
 	readonly APP_NAME: string;
 	readonly ACCESS_KEYS: string;
 	readonly PUBLIC_MAX_FILE_SIZE: string;
