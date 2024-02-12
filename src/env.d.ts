fcd237f00e5b15990b1ffee9788a942d6f0b9343
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-12 19:37:14 +0700

    Add WP_URL to env var defs

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
 	readonly APP_NAME: string;
 	readonly ACCESS_KEYS: string;
 	readonly PUBLIC_MAX_FILE_SIZE: string;
+	readonly WP_URL: string;
 }
 
 interface ImportMeta {
