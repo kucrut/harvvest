ec7f3d36a1eba9bb2c9153437ef7169aa06f80e2
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-26 03:53:52 +0700

    ContentWrap: Accept center prop

--- src/lib/components/content-wrap.svelte
+++ src/lib/components/content-wrap.svelte
@@ -1,1 +1,18 @@
-<main class="container"><slot /></main>
+<script>
+	export let center = false;
+</script>
+
+<main class="container" class:centered={center}><slot /></main>
+
+<style>
+	main {
+		padding-block: var( --pico-block-spacing-vertical );
+		min-height: 100dvh;
+
+		&.centered {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		}
+	}
+</style>
