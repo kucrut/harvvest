15bc71cc85310a48aae9773d53be51024be56133
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-26 02:56:57 +0700

    TextField: Remove tw classes

--- src/lib/components/text-field.svelte
+++ src/lib/components/text-field.svelte
@@ -3,12 +3,10 @@
 	export let label;
 	export let multiline = false;
 	export let value = '';
-
-	$: label_class = 'required' in $$restProps ? `after:content-['_*'] after:font-semibold after:text-error-400` : null;
 </script>
 
 <label class="label">
-	<span class={label_class}>{label}</span>
+	<span>{label}</span>
 	{#if multiline}
 		<textarea {value} class="textarea" {...$$restProps} on:blur on:change on:focus on:input />
 	{:else}
