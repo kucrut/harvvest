bccd3c9ceae954f374c71ba8621a3055fc132bdb
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-26 02:52:09 +0700

    Layout: Remove Drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,10 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-	import { Drawer, getDrawerStore, initializeStores } from '@skeletonlabs/skeleton';
-	import Alert from '$lib/components/alert.svelte';
-	import AppMenu from '$lib/components/app-menu.svelte';
+	import { initializeStores } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 
 	initializeStores();
-
-	const drawer_store = getDrawerStore();
 
 	onMount( () => {
 		const offline_path = '/offline';
@@ -28,13 +24,7 @@
 	} );
 </script>
 
-<Drawer>
-	{#if $drawer_store.id === 'app-menu'}
-		<AppMenu />
-	{:else if $drawer_store.id === 'alert'}
-		<Alert meta={$drawer_store.meta} on:dismiss={() => drawer_store.close()} />
-	{/if}
-</Drawer>
+<!-- Alert component -->
 
 <div class="app-shell">
 	<div class="app-bar">
