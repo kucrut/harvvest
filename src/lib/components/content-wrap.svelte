eb43e5e42c95be0fef865345cbf49a145b86c59b
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-26 04:30:50 +0700

    ContentWrap: Remove center prop

--- src/lib/components/content-wrap.svelte
+++ src/lib/components/content-wrap.svelte
@@ -1,18 +1,8 @@
-<script>
-	export let center = false;
-</script>
-
-<main class="container" class:centered={center}><slot /></main>
+<main class="container"><slot /></main>
 
 <style>
 	main {
 		padding-block: var( --pico-block-spacing-vertical );
 		min-height: 100dvh;
-
-		&.centered {
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-		}
 	}
 </style>
