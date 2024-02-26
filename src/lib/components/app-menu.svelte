4c2a1985b3906cc8b9ab9ecec1ccc724a39c1145
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-27 04:17:06 +0700

    AppMenu: Style close button

--- src/lib/components/app-menu.svelte
+++ src/lib/components/app-menu.svelte
@@ -7,7 +7,21 @@
 </script>
 
 <aside class:is-open={is_open}>
-	<button on:click={close}>Close</button>
+	<button class="close" on:click={close}
+		><svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="icon icon-tabler icon-tabler-x"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg
+		></button
+	>
 	<nav>
 		<h1>Photo Harvest</h1>
 		<ul>
@@ -54,6 +68,16 @@
 		&.is-open {
 			transform: translateZ( 0 );
 		}
+	}
+
+	.close {
+		position: absolute;
+		inset-block-start: var( --pico-spacing );
+		inset-inline-end: var( --pico-spacing );
+		background: unset;
+		border: unset;
+		padding: unset;
+		color: inherit;
 	}
 
 	h1 {
