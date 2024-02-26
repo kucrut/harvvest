2dd55715359ae39c086d8ea06255d48fdfb5b3cd
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-27 01:44:47 +0700

    Layout (server): Add app_name to data

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,12 +1,21 @@
+import { env } from '$env/dynamic/private';
+
 /** @type {import('./$types').LayoutServerLoad} */
 export const load = async ( { locals } ) => {
-	if ( locals.session ) {
-		return {
-			user: {
-				avatar_url: locals.session.avatar_url,
-				name: locals.session.name,
-				wp_url: locals.session.wp_url,
-			},
-		};
+	const data = {
+		app_name: env.APP_NAME,
+	};
+
+	if ( ! locals.session ) {
+		return data;
 	}
+
+	return {
+		...data,
+		user: {
+			avatar_url: locals.session.avatar_url,
+			name: locals.session.name,
+			wp_url: locals.session.wp_url,
+		},
+	};
 };
