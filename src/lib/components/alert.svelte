b69ce06e5fe7c9f8f3b67c1d061ba5284508ef79
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-27 00:20:57 +0700

    Alert: Add style

--- src/lib/components/alert.svelte
+++ src/lib/components/alert.svelte
@@ -2,6 +2,8 @@
 	import { createEventDispatcher, onMount } from 'svelte';
 
 	export let timeout = 2000;
+	/** @type { 'error' | 'message' | 'success' } */
+	export let type = 'message';
 
 	const dispatch = createEventDispatcher();
 
@@ -16,6 +18,31 @@
 	} );
 </script>
 
-<aside>
+<aside class={type}>
 	<slot />
 </aside>
+
+<style>
+	aside {
+		position: fixed;
+		bottom: 0;
+		block-size: fit-content;
+		inline-size: calc( 100dvw - ( var( --pico-spacing ) * 2 ) );
+		margin: var( --pico-spacing );
+		padding: var( --pico-spacing );
+		border: var( --pico-border-width ) solid var( --pico-muted-border-color );
+		border-radius: var( --pico-border-radius );
+
+		& > :last-child {
+			margin-block-end: unset;
+		}
+	}
+
+	.error {
+		border-left: 5px solid var( --pico-form-element-invalid-border-color );
+	}
+
+	.success {
+		border-left: 5px solid var( --pico-form-element-valid-border-color );
+	}
+</style>
