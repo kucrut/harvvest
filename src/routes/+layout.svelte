cbdfa70cba9e6ce92fc5a703c36fe5ac21f80ee3
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-27 01:45:00 +0700

    Layout: Bring back title & menu button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
 
+	export let data;
+
 	onMount( () => {
 		const offline_path = '/offline';
 
@@ -25,6 +27,56 @@
 <!-- Alert component -->
 
 <div>
-	<!-- TODO: Add menu button? -->
+	<hgroup class="container">
+		<h1>{data.app_name}</h1>
+		<button class="outline"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				aria-hidden="true"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path
+					d="M4 18l16 0"
+				/></svg
+			></button
+		>
+	</hgroup>
 	<slot />
 </div>
+
+<style>
+	hgroup {
+		display: grid;
+		align-items: center;
+		padding-block: var( --pico-spacing );
+	}
+
+	h1,
+	button {
+		grid-column: 1/1;
+		grid-row: 1/1;
+	}
+
+	h1 {
+		text-align: center;
+		font-size: 1.5rem;
+	}
+
+	button {
+		align-self: self-start;
+		inline-size: fit-content;
+		border: unset;
+		padding: unset;
+	}
+
+	svg {
+		display: inline-block;
+		vertical-align: middle;
+	}
+</style>
