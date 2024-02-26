9b75292634f98c421af194b9bbdcaf6ed240a1fe
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-26 19:27:10 +0700

    Add component: Alert

---
+++ src/lib/components/alert.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { createEventDispatcher, onMount } from 'svelte';
+
+	export let timeout = 2000;
+
+	const dispatch = createEventDispatcher();
+
+	onMount( () => {
+		if ( timeout < 500 ) {
+			return;
+		}
+
+		setTimeout( () => {
+			dispatch( 'expire' );
+		}, timeout );
+	} );
+</script>
+
+<aside>
+	<slot />
+</aside>
