6416c1c3374f6151bebb275317112159a5698e56
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-28 16:39:26 +0700

    Logout page (server): Redirect to /login

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -2,9 +2,11 @@ import { clear_cookies } from '$lib/utils.server.js';
 import { delete_app_password, get_app_passwords } from '@kucrut/wp-api-helpers';
 import { redirect } from '@sveltejs/kit';
 
+const redirect_path = '/login';
+
 /** @type {import('./$types').PageServerLoad} */
 export const load = () => {
-	redirect( 307, '/' );
+	redirect( 307, redirect_path );
 };
 
 /** @type {import('./$types').Actions} */
@@ -12,7 +14,7 @@ export const actions = {
 	async default( { cookies, locals } ) {
 		if ( ! locals.session ) {
 			clear_cookies( cookies );
-			redirect( 302, '/login' );
+			redirect( 302, redirect_path );
 		}
 
 		const app_id = cookies.get( 'app_id' );
@@ -21,7 +23,7 @@ export const actions = {
 			// eslint-disable-next-line no-console
 			console.warn( '[FIXME] Logout action: `app_id` not found in cookies.' );
 			clear_cookies( cookies );
-			redirect( 302, '/login' );
+			redirect( 302, redirect_path );
 		}
 
 		// Wrapped in try/catch to prevent errors from leaking in case (for example) the app password
@@ -38,7 +40,7 @@ export const actions = {
 			console.error( 'Logout action: ', error );
 		} finally {
 			clear_cookies( cookies );
-			redirect( 302, '/login' );
+			redirect( 302, redirect_path );
 		}
 	},
 };
