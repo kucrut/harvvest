167b060266d7cdbd1e416c00fdc5028fd96d9e81
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-02 11:24:50 +0700

    Add component: Icon

---
+++ src/lib/components/icon.svelte
@@ -0,0 +1,8 @@
+<script>
+	/** @type {{name: string} & import("svelte/elements").SVGAttributes<EventTarget>} */
+	const { name, ...rest } = $props();
+</script>
+
+<svg {...rest}>
+	<use href="#icon-{name}" />
+</svg>
