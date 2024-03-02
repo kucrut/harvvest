15b1f842a4c1470e01993354a319f6d587049f9e
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-03 00:38:56 +0700

    Upload form: Split file input into separate component

---
+++ src/lib/components/media-upload-field.svelte
@@ -0,0 +1,89 @@
+<script>
+	import { create_data_uri, generate_file_id } from '$lib/utils.js';
+	import pretty_bytes from 'pretty-bytes';
+	import Icon from './icon.svelte';
+
+	/**
+	 * @type {{
+	 *   files?: FileList;
+	 *   max_file_size: number;
+	 *   onpreviewerror?: (error: unknown, file: File) => void;
+	 * } & Omit<import('svelte/elements').HTMLInputAttributes, 'accept' | 'class' | 'required' | 'type' > }
+	 */
+	let { files, max_file_size, onpreviewerror, ...rest } = $props();
+
+	/** @type {'image'|'video'|undefined} */
+	let file_type = $state( undefined );
+	let last_selected_file = $state( '' );
+	let preview_src = $state( '' );
+
+	$effect( () => {
+		if ( ! files?.length ) {
+			last_selected_file = '';
+			preview_src = '';
+
+			return;
+		}
+
+		const file = files[ 0 ];
+		const file_id = generate_file_id( file );
+
+		if ( last_selected_file === file_id ) {
+			return;
+		}
+
+		( async () => {
+			try {
+				if ( file.type.startsWith( 'image/' ) ) {
+					const uri = await create_data_uri( file );
+					preview_src = uri;
+					file_type = 'image';
+				} else if ( file.type.startsWith( 'video/' ) ) {
+					file_type = 'video';
+				} else {
+					file_type = undefined;
+				}
+			} catch ( error ) {
+				preview_src = '';
+
+				if ( onpreviewerror ) {
+					onpreviewerror( error, file );
+				}
+			} finally {
+				last_selected_file = file_id;
+			}
+		} )();
+	} );
+</script>
+
+<label>
+	{#if file_type === 'image' && preview_src}
+		<img alt="" src={preview_src} />
+	{:else if file_type === 'video'}
+		<div><Icon name="file-video" width="72" height="72" /></div>
+	{/if}
+
+	<small>Click to select an image/video. Maximum file size is <em>{pretty_bytes( max_file_size )}</em>.</small>
+
+	<!-- NOTE: A hack on the required attribute is needed so that we can re-use the file shared to our PWA. -->
+	<input {...rest} class="visually-hidden" required={! files?.length} accept="image/*,video/*" type="file" bind:files />
+</label>
+
+<style>
+	label {
+		border: var( --pico-border-width ) dashed var( --pico-form-element-border-color );
+		border-radius: var( --pico-border-radius );
+		padding: var( --pico-form-element-spacing-vertical ) var( --pico-form-element-spacing-horizontal );
+		background-color: var( --pico-form-element-background-color );
+		text-align: center;
+	}
+
+	img {
+		max-width: 100%;
+		height: auto;
+		border-radius: var( --pico-border-radius );
+		display: block;
+		margin-inline: auto;
+		margin-block: var( --pico-spacing );
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,26 @@
 <script>
 	import { applyAction, enhance } from '$app/forms';
-	import { create_data_uri, generate_file_id, remove_file_extension } from '$lib/utils.js';
+	import { remove_file_extension } from '$lib/utils.js';
 	import { get_error_message } from '@kucrut/wp-api-helpers/utils';
 	import { page } from '$app/stores';
 	import { retrieve_pwa_shared_file } from '$lib/utils.client.js';
 	import pretty_bytes from 'pretty-bytes';
 	import Alert from '$lib/components/alert.svelte';
 	import CopyButton from '$lib/components/copy-button.svelte';
-	import Icon from '$lib/components/icon.svelte';
 	import Main from '$lib/components/main.svelte';
 	import TextField from '$lib/components/text-field.svelte';
 	import TermsField from '$lib/components/terms-field.svelte';
+	import MediaUploadField from '$lib/components/media-upload-field.svelte';
 
 	const { data, form } = $props();
 
 	/** @type {import('$types').Alert|null} */
 	let alert = $state( null );
 	/** @type {'image'|'video'|undefined} */
-	let file_type = $state( undefined );
 	/** @type {FileList|undefined} */
 	let files = $state( undefined );
 	let has_title_touched = $state( false );
 	let is_submitting = $state( false );
-	let last_selected_file = $state( '' );
-	let preview_src = $state( '' );
 	let title = $state( '' );
 
 	const max_file_size_formatted = $derived( pretty_bytes( data.max_file_size || 0 ) );
@@ -45,8 +42,6 @@
 				formElement.reset();
 				files = undefined;
 				has_title_touched = false;
-				last_selected_file = '';
-				preview_src = '';
 			}
 		};
 	};
@@ -102,68 +97,22 @@
 			history.replaceState( '', '', '/' );
 		} )();
 	} );
-
-	$effect( () => {
-		if ( ! files?.length ) {
-			last_selected_file = '';
-			preview_src = '';
-
-			return;
-		}
-
-		const file = files[ 0 ];
-		const file_id = generate_file_id( file );
-
-		if ( last_selected_file === file_id ) {
-			return;
-		}
-
-		( async () => {
-			try {
-				if ( file.type.startsWith( 'image/' ) ) {
-					const uri = await create_data_uri( file );
-					preview_src = uri;
-					file_type = 'image';
-				} else if ( file.type.startsWith( 'video/' ) ) {
-					file_type = 'video';
-				} else {
-					file_type = undefined;
-				}
-			} catch ( error ) {
-				preview_src = '';
-				alert = {
-					message: get_error_message( error, 'Failed to create preview image.', false ),
-					type: 'error',
-				};
-			} finally {
-				last_selected_file = file_id;
-			}
-		} )();
-	} );
 </script>
 
 <Main>
 	<form enctype="multipart/form-data" method="POST" use:enhance={handle_submit}>
-		<label>
-			{#if file_type === 'image' && preview_src}
-				<img alt="" src={preview_src} />
-			{:else if file_type === 'video'}
-				<div><Icon name="file-video" width="72" height="72" /></div>
-			{/if}
-			<small>Click to select an image/video. Maximum file size is <em>{max_file_size_formatted}</em>.</small>
-
-			<span class="visually-hidden">
-				<!-- NOTE: A hack on the required attribute is needed so that we can re-use the file shared to our PWA. -->
-				<input
-					required={! files?.length}
-					accept="image/*,video/*"
-					disabled={is_submitting}
-					name="file"
-					type="file"
-					bind:files
-				/>
-			</span>
-		</label>
+		<MediaUploadField
+			bind:files
+			disabled={is_submitting}
+			max_file_size={data.max_file_size || 0}
+			name="file"
+			onpreviewerror={error => {
+				alert = {
+					message: get_error_message( error, 'Failed to create preview image.', false ),
+					type: 'error',
+				};
+			}}
+		/>
 		<TextField multiline required label="Alternative text" name="alt_text" />
 		<TextField required label="Caption" name="caption" />
 		<TextField label="Title" name="title" bind:value={title} on:focus={() => ( has_title_touched = true )} />
@@ -189,21 +138,3 @@
 		{/if}
 	</Alert>
 {/if}
-
-<style>
-	label {
-		border: var( --pico-border-width ) dashed var( --pico-form-element-border-color );
-		border-radius: var( --pico-border-radius );
-		padding: var( --pico-form-element-spacing-vertical ) var( --pico-form-element-spacing-horizontal );
-		background-color: var( --pico-form-element-background-color );
-		text-align: center;
-	}
-
-	img {
-		max-width: 100%;
-		height: auto;
-		border-radius: var( --pico-border-radius );
-		display: block;
-		margin-block-end: var( --pico-spacing );
-	}
-</style>
