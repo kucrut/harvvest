319e6673185b8feb0be70f40ef6392a4d466c603
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-02 08:50:27 +0700

    Sidebar: Replace svg with lucide icon

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import CloseIcon from 'lucide-svelte/icons/x';
 	import Nav from './nav.svelte';
 	import UserInfo from './user-info.svelte';
 
@@ -8,20 +9,7 @@
 </script>
 
 <aside class:is-open={is_open}>
-	<button on:click={close}
-		><svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			fill="none"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg
-		></button
-	>
+	<button on:click={close}><CloseIcon /></button>
 
 	<Nav />
 
