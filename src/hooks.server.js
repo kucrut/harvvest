560b1b800b59a6bc98239a217377933e35a08b99
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-02 21:20:32 +0700

    Hooks (server): Check session validity

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,18 @@
 import { delete_session_cookies, get_session } from '$lib/utils.server.js';
+import { get_current_app_password } from '@kucrut/wp-api-helpers';
 import { sequence } from '@sveltejs/kit/hooks';
 
 /** @type {import('@sveltejs/kit').Handle} */
 async function check_session( { event, resolve } ) {
 	try {
-		event.locals.session = get_session( event.cookies );
+		const session = get_session( event.cookies );
+
+		if ( ! session ) {
+			return await resolve( event );
+		}
+
+		await get_current_app_password( session.api_url, session.auth );
+		event.locals.session = session;
 	} catch {
 		delete_session_cookies( event.cookies );
 	}
