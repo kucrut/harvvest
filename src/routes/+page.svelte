b38aba4b29384745772ea869e70685d0e5ca1976
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-02 08:58:20 +0700

    Upload form: Replace svg with lucide icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
 	import Main from '$lib/components/main.svelte';
 	import TextField from '$lib/components/text-field.svelte';
 	import TermsField from '$lib/components/terms-field.svelte';
+	import VideoIcon from 'lucide-svelte/icons/file-video';
 
 	const { data, form } = $props();
 
@@ -147,25 +148,7 @@
 			{#if file_type === 'image' && preview_src}
 				<img alt="" src={preview_src} />
 			{:else if file_type === 'video'}
-				<div>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="w-14"
-						fill="none"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						><path d="M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /><rect
-							width="8"
-							height="6"
-							x="2"
-							y="12"
-							rx="1"
-						/><path d="m10 15.5 4 2.5v-6l-4 2.5" /></svg
-					>
-				</div>
+				<div><VideoIcon size="72" /></div>
 			{/if}
 			<!-- NOTE: A hack on the required attribute is needed so that we can re-use the file shared to our PWA. -->
 			<input
