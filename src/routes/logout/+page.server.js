1875de4c8cfc771e4868afa5907ce5cd31bb2d52
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-02 21:46:40 +0700

    Logout handler: Use uuid from session data to delete app password

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -1,5 +1,5 @@
-import { APP_ID_COOKIE_NAME, clear_cookies } from '$lib/utils.server.js';
-import { delete_app_password, get_app_passwords } from '@kucrut/wp-api-helpers';
+import { clear_cookies } from '$lib/utils.server.js';
+import { delete_app_password } from '@kucrut/wp-api-helpers';
 import { redirect } from '@sveltejs/kit';
 
 const redirect_path = '/login';
@@ -17,24 +17,10 @@ export const actions = {
 			redirect( 302, redirect_path );
 		}
 
-		const app_id = cookies.get( APP_ID_COOKIE_NAME );
-
-		if ( ! app_id ) {
-			// eslint-disable-next-line no-console
-			console.warn( '[FIXME] Logout action: `app_id` not found in cookies.' );
-			clear_cookies( cookies );
-			redirect( 302, redirect_path );
-		}
-
 		// Wrapped in try/catch to prevent errors from leaking in case
 		// (for example) the app password has been manually deleted.
 		try {
-			const all_app_pass = await get_app_passwords( locals.session.api_url, locals.session.auth, 'me', 'edit' );
-			const app_pass = all_app_pass.find( item => item.app_id === app_id );
-
-			if ( app_pass ) {
-				await delete_app_password( locals.session.api_url, locals.session.auth, 'me', app_pass.uuid );
-			}
+			await delete_app_password( locals.session.api_url, locals.session.auth, 'me', locals.session.auth_uuid );
 		} catch ( error ) {
 			// eslint-disable-next-line no-console
 			console.error( 'Logout action: ', error );
