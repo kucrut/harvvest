b0cbedeb7b878f7b4b556fbcf9647ed1d57ca178
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-05 03:02:17 +0700

    Add component: IconButton

---
+++ src/lib/components/icon-button.svelte
@@ -0,0 +1,24 @@
+<script>
+	import clsx from 'clsx/lite';
+	import Icon from './icon.svelte';
+
+	/**
+	 * @type {{
+	 *   height?: number;
+	 *   icon: string;
+	 *   label: string
+	 *   width?: number;
+	 * } & import('svelte/elements').HTMLAttributes<HTMLButtonElement> }
+	 */
+	const { class: cls = '', height, icon, label, width, ...rest } = $props();
+</script>
+
+<button aria-label={label} class={clsx( 'outline', cls )} {...rest}><Icon {height} {width} name={icon} /></button>
+
+<style>
+	button {
+		size: fit-content;
+		border: unset;
+		padding: unset;
+	}
+</style>
