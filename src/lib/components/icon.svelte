066953f1460ffc08148169c8435cdae3ce3076f0
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-05 02:59:14 +0700

    Icon: Update props typing

--- src/lib/components/icon.svelte
+++ src/lib/components/icon.svelte
@@ -1,6 +1,6 @@
 <script>
 	/** @type {{name: string} & import("svelte/elements").SVGAttributes<EventTarget>} */
-	const { name, 'aria-hidden': ah = true, height = '24', role = 'presentation', width = '24', ...rest } = $props();
+	const { name, 'aria-hidden': ah = true, height = 24, role = 'presentation', width = 24, ...rest } = $props();
 </script>
 
 <svg aria-hidden={ah} {height} {role} {width} {...rest}>
