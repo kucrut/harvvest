b263176d5489794084d18f754178802bdfd86064
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-04 08:24:09 +0700

    Hooks: Inject svg sprite

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,6 +2,7 @@ import { delete_session_cookies, get_session } from '$lib/utils.server.js';
 import { get_current_app_password } from '@kucrut/wp-api-helpers';
 import { sequence } from '@sveltejs/kit/hooks';
 import { ZodError } from 'zod';
+import svg_sprite from '$lib/components/svg-sprite.svg?raw';
 
 /** @type {import('@sveltejs/kit').Handle} */
 async function check_session( { event, resolve } ) {
@@ -25,4 +26,13 @@ async function check_session( { event, resolve } ) {
 	return await resolve( event );
 }
 
-export const handle = sequence( check_session );
+/** @type {import('@sveltejs/kit').Handle} */
+function transform_html( { event, resolve } ) {
+	return resolve( event, {
+		transformPageChunk: ( { html } ) => {
+			return html.replace( '%svg_sprite%', `<div class="svg-sprite">${ svg_sprite }</div>` );
+		},
+	} );
+}
+
+export const handle = sequence( check_session, transform_html );
