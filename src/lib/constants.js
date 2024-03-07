f647c6695765fc66d3cc5499a2d53c1f4fb1e36c
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-07 11:01:35 +0700

    Add TODO

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
 export const PWA_SHARE_READY_ACTION = 'share-ready';
 export const PWA_SHARE_TARGET_PARAM = 'share';
-export const PWA_SHARE_TARGET_UPLOAD_MEDIA_ROUTE = '/';
+export const PWA_SHARE_TARGET_UPLOAD_MEDIA_ROUTE = '/'; // TODO: Change this to /upload-media
 export const PWA_SHARE_TARGET_UPLOAD_MEDIA_PATH = `${ PWA_SHARE_TARGET_UPLOAD_MEDIA_ROUTE }?${ PWA_SHARE_TARGET_PARAM }`;
