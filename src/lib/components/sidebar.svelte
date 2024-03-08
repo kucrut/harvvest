a13400a0cf25b0bbdce29f77b602c1f6bae4db02
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-08 09:14:15 +0700

    Sidebar: Use own rune

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,15 +1,13 @@
 <script>
 	import { page } from '$app/stores';
+	import { sidebar } from '$lib/components/sidebar.svelte.js';
 	import IconButton from '$lib/components/icon-button.svelte';
 	import Nav from './nav.svelte';
 	import UserInfo from './user-info.svelte';
-
-	/** @type {{ close: () => void; is_open?: boolean }} */
-	const { close, is_open = false } = $props();
 </script>
 
-<aside class:is-open={is_open}>
-	<IconButton class="close" icon="x" label="Close sidebar" onclick={close} />
+<aside class:is-open={sidebar.is_open}>
+	<IconButton class="close" icon="x" label="Close sidebar" onclick={() => sidebar.toggle()} />
 
 	<Nav />
 
