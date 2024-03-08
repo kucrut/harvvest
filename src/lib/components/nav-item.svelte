745bed1c2df639cdb9683ecbc526f444222bf869
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-08 09:23:22 +0700

    Add component: NavItem

---
+++ src/lib/components/nav-item.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { sidebar } from './sidebar.svelte.js';
+	import Icon from './icon.svelte';
+
+	/** @type {{icon: string; href:string; label: string}} */
+	const { icon, href, label } = $props();
+
+	function handle_click() {
+		if ( href.startsWith( '/' ) ) {
+			sidebar.toggle();
+		}
+	}
+</script>
+
+<li>
+	<a {href} onclick={handle_click}>
+		<Icon name={icon} />{label}
+	</a>
+</li>
+
+<style>
+	a {
+		display: inline-flex;
+		align-items: center;
+		gap: 0.25rem;
+	}
+</style>
