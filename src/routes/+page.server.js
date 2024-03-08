38a07abcd727b86a1f7ed9d6ecc67d5f86ffd96e
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-08 08:35:32 +0700

    Home page loader: Redirect to /login if there's no session

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,10 @@
-export function load() {
+import { redirect } from '@sveltejs/kit';
+
+export function load( { locals } ) {
+	if ( ! locals.session ) {
+		redirect( 301, '/login' );
+	}
+
 	return {
 		hide_title: true,
 		meta: {
