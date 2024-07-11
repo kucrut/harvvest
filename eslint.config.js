c942e44265a0935534742b1a92ca14f96df1e666
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-07-12 01:03:48 +0700

    ESLint config: Remove prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,11 @@
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
-import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
 	...svelte.configs[ 'flat/recommended' ],
-	prettier,
-	...svelte.configs[ 'flat/prettier' ],
 	{
 		languageOptions: {
 			globals: {
