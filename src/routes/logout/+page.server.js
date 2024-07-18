8edcca96e62001cc875cd72afad51aec2d3fe88f
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-07-19 01:15:46 +0700

    Logout route: Simplify handler

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -2,21 +2,9 @@ import { clear_cookies } from '$lib/utils.server.js';
 import { delete_app_password } from '@kucrut/wp-api-helpers';
 import { redirect } from '@sveltejs/kit';
 
-const redirect_path = '/login';
-
 /** @type {import('./$types').PageServerLoad} */
-export const load = () => {
-	redirect( 307, redirect_path );
-};
-
-/** @type {import('./$types').Actions} */
-export const actions = {
-	async default( { cookies, locals } ) {
-		if ( ! locals.session ) {
-			clear_cookies( cookies );
-			redirect( 302, redirect_path );
-		}
-
+export async function load( { cookies, locals } ) {
+	if ( locals.session ) {
 		// Wrapped in try/catch to prevent errors from leaking in case
 		// (for example) the app password has been manually deleted.
 		try {
@@ -24,9 +12,10 @@ export const actions = {
 		} catch ( error ) {
 			// eslint-disable-next-line no-console
 			console.error( 'Logout action: ', error );
-		} finally {
-			clear_cookies( cookies );
-			redirect( 302, redirect_path );
 		}
-	},
-};
+	}
+
+	clear_cookies( cookies );
+	redirect( 302, '/login' );
+}
+
