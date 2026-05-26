3a4899aceae5a1c3e76e2ae51ca39c7a0868fad2
Dzikri Aziz <dzikri.aziz@gmail.com>
2026-05-26 22:16:32 +0700

    Convert to ts

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,21 +1,25 @@
 import { clear_cookies } from '$lib/utils.server.js';
 import { delete_app_password } from '@kucrut/wp-api-helpers';
 import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-/** @type {import('./$types').PageServerLoad} */
-export async function load( { cookies, locals } ) {
+export const load = ( async ( { cookies, locals } ) => {
 	if ( locals.session ) {
 		// Wrapped in try/catch to prevent errors from leaking in case
 		// (for example) the app password has been manually deleted.
 		try {
-			await delete_app_password( locals.session.api_url, locals.session.auth, 'me', locals.session.auth_uuid );
+			await delete_app_password(
+				locals.session.api_url,
+				locals.session.auth,
+				'me',
+				locals.session.auth_uuid,
+			);
 		} catch ( error ) {
 			// eslint-disable-next-line no-console
-			console.error( 'Logout action: ', error );
+			console.error( 'Logout action:', error );
 		}
 	}
 
 	clear_cookies( cookies );
 	redirect( 302, '/login' );
-}
-
+} ) satisfies PageServerLoad;
