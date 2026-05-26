1a6daafd930eb53d367e8711f920bf43cbe13a35
Dzikri Aziz <dzikri.aziz@gmail.com>
2026-05-26 22:53:17 +0700

    TermsField: Convert to TS

--- src/lib/components/terms-field.svelte
+++ src/lib/components/terms-field.svelte
@@ -1,11 +1,15 @@
-<script>
-	/** @type {{ taxonomy: import('$types').Taxonomy_Terms_Option }} */
-	const { taxonomy } = $props();
+<script lang="ts">
+	import type { Taxonomy_Terms_Option } from '$types';
+
+	interface Props {
+		taxonomy: Taxonomy_Terms_Option
+	}
+
+	const { taxonomy }: Props = $props();
 </script>
 
 <fieldset>
 	<legend>{taxonomy.name}</legend>
-	<!-- eslint-disable-next-line space-in-parens -->
 	{#each taxonomy.terms as term ( `${ term.name }-${ term.id }` )}
 		<label>
 			<input name="{taxonomy.slug}[]" type="checkbox" value={term.id} />
