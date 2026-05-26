2778ab1507cbf6e11abea57ba13cf931a2c35a2f
Dzikri Aziz <dzikri.aziz@gmail.com>
2026-05-26 22:54:27 +0700

    TextField: Convert to TS

--- src/lib/components/text-field.svelte
+++ src/lib/components/text-field.svelte
@@ -1,12 +1,13 @@
-<script>
-	/**
-	 * @type {{
-	 *   label: string;
-	 *   multiline?: boolean;
-	 *   value?: string;
-	 * } & import('svelte/elements').HTMLInputAttributes & import('svelte/elements').HTMLTextareaAttributes }
-	 */
-	let { label, multiline = false, value = $bindable( '' ), ...rest } = $props();
+<script lang="ts">
+	import type { HTMLInputAttributes, HTMLTextareaAttributes } from 'svelte/elements';
+
+	type Props = {
+		label: string;
+		multiline?: boolean;
+		value?: string;
+	} & HTMLInputAttributes & HTMLTextareaAttributes;
+
+	let { label, multiline = false, value = $bindable( '' ), ...rest }: Props = $props();
 </script>
 
 <label>
@@ -19,7 +20,7 @@
 </label>
 
 <style>
-	label:has( [required] ) span::after {
+	label:has([required]) span::after {
 		content: '\00a0\*';
 		color: var( --pico-form-element-invalid-border-color );
 	}
